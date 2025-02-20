37af3a3a27dd63e184581a213f385f9986d86281
tomasBONILL <tomasedubonilla@gmail.com>
2025-02-20 09:25:48 -0600

    Primer consejo realizado

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const texto =  document.createElement("h1")
-texto.innerText = "Consejos?"
+texto.innerText = "Consejos para sobrevivir a teacher Irvin"
 
 document.body.appendChild(texto);
 
 const lista = document.createElement("li")
+lista.innerText = "Ser puntual con las tareas"
+
+document.body.appendChild(lista);
