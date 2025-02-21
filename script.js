e96d7df62a4c3faa8f3f6780c914971fdf856183
DaniloMCD <danilo.granados2026@hilasal.superate.org.sv>
2025-02-20 21:37:13 -0600

    Segundo consejo hecho

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const text = document.createTextNode("Consejo 1: Pon atencion en clase, pero div
 
 li.appendChild(text)
 document.body.appendChild(li)
+
+//Consejo 2
+const li2 = document.createElement("li")
+const text2 = document.createTextNode("Consejo 2: Siempre entrega las taras puntualmente.")
+
+li2.appendChild(text2)
+document.body.appendChild(li2)
